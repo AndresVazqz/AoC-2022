e1bfb0c8f8896129b718886da185aaa3720f50e9
Andres Vazquez Palomo <vazqua23@emea.roche.com>
2022-12-03 19:58:46 +0100

    cleaned code

--- Day01/code.ts
+++ Day01/code.ts
@@ -12,11 +12,9 @@ elfs.forEach(x => {
 })
 
 const max1 = Math.max(...result);
-var pos1 = result.indexOf(max1);
-result.splice(pos1, 1);
+result.splice(result.indexOf(max1), 1);
 const max2 = Math.max(...result);
-var pos2 = result.indexOf(max2);
-result.splice(pos2, 1);
+result.splice(result.indexOf(max2), 1);
 const max3 = Math.max(...result);
 
 
