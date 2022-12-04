bf3c1fca825183bf63e9309d815bcd5f18a53086
Andres Vazquez Palomo <vazqua23@emea.roche.com>
2022-12-04 10:19:12 +0100

    day03

---
+++ Day03/code-2.ts
@@ -0,0 +1,36 @@
+import { readFileSync } from 'fs';
+
+const input: string = readFileSync('input.txt','utf8');
+    
+const alphaMayus = Array.from(Array(26)).map((e, i) => i + 65);
+const alphabetMayus = alphaMayus.map((x) => String.fromCharCode(x));
+
+const alpha = Array.from(Array(26)).map((e, i) => i + 97);
+const alphabet = alpha.map((x) => String.fromCharCode(x));
+
+var elfs = input.split(/\r?\n/);
+
+var sum = 0;
+
+for (let i = 0; i < elfs.length; i += 3) {
+    var first = [...elfs[i]];
+    var second = [...elfs[i + 1]];
+    var third = [...elfs[i + 2]];
+    
+    var j = 0;
+    var found = false;
+    while (!found) {
+        if (second.includes(first[j]) && third.includes(first[j])) {
+            found = true;
+            if (first[j] >= 'a' &&  first[j] <= 'z') {
+                sum += alphabet.indexOf(first[j]) +1
+            }
+            else if (first[j] >= 'A' &&  first[j] <= 'Z') {
+                sum += alphabetMayus.indexOf(first[j]) +27
+            }
+        }
+        ++j;
+    }
+}
+
+console.log('Result 2: ' + sum);
